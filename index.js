c4e7be3eb69a9a5e370a37b0f2dc2ad2ec0bbf92
SirGalleto <sir@galleto.xyz>
2016-11-15 14:43:23 -0600

    (update) add uid validation

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  console.log('Connected');
-  socket.emit('pong');
-
-  socket.on('disconnect', function () {
-    console.log('Disconnected');
+  socket.on('pingServer', function(data) {
+    socket.emit('pong', data);
   });
 });
